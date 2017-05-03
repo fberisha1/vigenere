acc8ec2fd2331fb757356f59b7077e43e1f933c7
fberisha1 <fisnik.f.berisha@gmail.com>
2017-05-03 19:52:49 +0200

    Metod
    Metoda per gjetjen e gjatesis se qelesit

--- vigenere/Form1.Designer.cs
+++ vigenere/Form1.Designer.cs
@@ -28,12 +28,108 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnDekripto = new System.Windows.Forms.Button();
+            this.lblCelsi1 = new System.Windows.Forms.Label();
+            this.txtCipher = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btnCeles = new System.Windows.Forms.Button();
+            this.lblCelesi = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnDekripto
+            // 
+            this.btnDekripto.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDekripto.Location = new System.Drawing.Point(349, 441);
+            this.btnDekripto.Name = "btnDekripto";
+            this.btnDekripto.Size = new System.Drawing.Size(113, 46);
+            this.btnDekripto.TabIndex = 10;
+            this.btnDekripto.Text = "Dekripto";
+            this.btnDekripto.UseVisualStyleBackColor = true;
+            this.btnDekripto.Click += new System.EventHandler(this.btnDekripto_Click);
+            // 
+            // lblCelsi1
+            // 
+            this.lblCelsi1.AutoSize = true;
+            this.lblCelsi1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCelsi1.Location = new System.Drawing.Point(29, 208);
+            this.lblCelsi1.Name = "lblCelsi1";
+            this.lblCelsi1.Size = new System.Drawing.Size(0, 24);
+            this.lblCelsi1.TabIndex = 8;
+            // 
+            // txtCipher
+            // 
+            this.txtCipher.Location = new System.Drawing.Point(12, 38);
+            this.txtCipher.Name = "txtCipher";
+            this.txtCipher.Size = new System.Drawing.Size(1180, 136);
+            this.txtCipher.TabIndex = 7;
+            this.txtCipher.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 11);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(116, 24);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Ciphertexti:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(33, 400);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(432, 20);
+            this.textBox1.TabIndex = 11;
+            // 
+            // btnCeles
+            // 
+            this.btnCeles.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCeles.Location = new System.Drawing.Point(900, 180);
+            this.btnCeles.Name = "btnCeles";
+            this.btnCeles.Size = new System.Drawing.Size(292, 46);
+            this.btnCeles.TabIndex = 12;
+            this.btnCeles.Text = "Gjej celesat e mundshem";
+            this.btnCeles.UseVisualStyleBackColor = true;
+            this.btnCeles.Click += new System.EventHandler(this.btnCeles_Click);
+            // 
+            // lblCelesi
+            // 
+            this.lblCelesi.AutoSize = true;
+            this.lblCelesi.Location = new System.Drawing.Point(30, 374);
+            this.lblCelesi.Name = "lblCelesi";
+            this.lblCelesi.Size = new System.Drawing.Size(133, 13);
+            this.lblCelesi.TabIndex = 13;
+            this.lblCelesi.Text = "Shkruaj njerin nga Celesat:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1252, 602);
+            this.Controls.Add(this.lblCelesi);
+            this.Controls.Add(this.btnCeles);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btnDekripto);
+            this.Controls.Add(this.lblCelsi1);
+            this.Controls.Add(this.txtCipher);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnDekripto;
+        private System.Windows.Forms.Label lblCelsi1;
+        private System.Windows.Forms.RichTextBox txtCipher;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btnCeles;
+        private System.Windows.Forms.Label lblCelesi;
     }
 }
 
--- vigenere/Form1.cs
+++ vigenere/Form1.cs
@@ -16,5 +16,238 @@ namespace vigenere
         {
             InitializeComponent();
         }
+
+        private void btnDekripto_Click(object sender, EventArgs e)
+        {
+
+        }
+        static List<string> NdajeTekstinMeGjatesiTeQelesit(string teksti, int gjatesiaQelesit)
+        {
+            List<string> rezultati = new List<string>();
+            StringBuilder[] sb = new StringBuilder[gjatesiaQelesit];
+
+            for (int i = 0; i < gjatesiaQelesit; i++)
+            {
+                sb[i] = new StringBuilder();
+            }
+
+            for (int i = 0; i < teksti.Length; i++)
+            {
+                sb[i % gjatesiaQelesit].Append(teksti[i]);
+            }
+
+            foreach (var item in sb)
+            {
+                rezultati.Add(item.ToString());
+            }
+
+            return rezultati;
+        }
+        static Dictionary<char, double> AnalizoFrekuencen(string teksti)
+        {
+            if (teksti == null)
+            {
+                return null;
+            }
+
+            Dictionary<char, double> Frekuenca_e_Paraqitjes = new Dictionary<char, double>();
+            int gjatesiaTekstit = teksti.Length;
+
+            for (int i = 0; i < gjatesiaTekstit; i++)
+            {
+                char c = teksti[i];
+                char qelesi = '#';
+
+                if ((c >= 'a' && c <= 'z'))
+                {
+                    qelesi = c;
+                }
+
+                if (c >= 'A' && c <= 'Z')
+                {
+                    qelesi = (char)(c + 'a' - 'A');
+                }
+
+                if (Frekuenca_e_Paraqitjes.Keys.Contains(qelesi))
+                {
+                    Frekuenca_e_Paraqitjes[qelesi] = Frekuenca_e_Paraqitjes[qelesi] + 1;
+                }
+                else
+                {
+                    Frekuenca_e_Paraqitjes[qelesi] = 1;
+                }
+            }
+
+            List<char> qelesit = Frekuenca_e_Paraqitjes.Keys.ToList();
+
+            foreach (char c in qelesit)
+            {
+                Frekuenca_e_Paraqitjes[c] /= gjatesiaTekstit;
+            }
+
+            return Frekuenca_e_Paraqitjes;
+        }
+        public string GjejQelesin(int gjatesia_qelesit)
+        {
+            string VigenereCryptogram;
+
+            VigenereCryptogram = txtCipher.Text;
+            VigenereCryptogram = VigenereCryptogram.Replace(" ", "");
+
+            int qelsi_gjatesia = gjatesia_qelesit;
+
+            Dictionary<char, double> Frekuenca_e_Paraqitjes;
+            List<char> QelesiChars = new List<char>();
+
+            VigenereCryptogram.Replace(" ", "").Replace("\n", "").Replace("\r", "");
+
+            List<string> TekstiZevendesues = NdajeTekstinMeGjatesiTeQelesit(VigenereCryptogram, qelsi_gjatesia);
+
+            foreach (string ceasarCryptogram in TekstiZevendesues)
+            {
+                Frekuenca_e_Paraqitjes = AnalizoFrekuencen(ceasarCryptogram);
+
+                double FrekuencaMaksimale = Frekuenca_e_Paraqitjes.Values.Max();
+
+                char MaksimumChar = Frekuenca_e_Paraqitjes.Keys.Where(c => Frekuenca_e_Paraqitjes[c] == FrekuencaMaksimale).FirstOrDefault();
+
+                QelesiChars.Add((char)(MaksimumChar - 'e' + 'A'));
+            }
+
+            string VigenereQelesi = "";
+
+            foreach (char c in QelesiChars)
+            {
+                VigenereQelesi += c;
+            }
+
+            return VigenereQelesi;
+        }
+
+        private void btnCeles_Click(object sender, EventArgs e)
+        {
+            string TekstiPaZbraztira;
+            TekstiPaZbraztira = txtCipher.Text;
+            TekstiPaZbraztira = TekstiPaZbraztira.Replace(" ", "");
+
+            var array1 = TekstiPaZbraztira.ToArray();
+
+            int[] NumratCelesa = new int[0];
+            int[] NumratPerseritur = new int[0];
+
+            int gjatesia = array1.Length;
+
+            for (int i = 0; i < (gjatesia - 2); i++)
+            {
+                for (int j = i; j < (gjatesia - 2); j++)
+                {
+
+                    if ((array1[i] == array1[j]) && (array1[i + 1] == array1[j + 1]) && (array1[i + 2] == array1[j + 2]))
+                    {
+                        Array.Resize(ref NumratCelesa, NumratCelesa.Length + 1);
+                        NumratCelesa[NumratCelesa.Length - 1] = j - i;
+                    }
+                }
+            }
+
+            int Gjatesia = NumratCelesa.Length;
+
+            for (int i = 0; i < Gjatesia; i++)
+            {
+                for (int j = 2; j < (int)((NumratCelesa[i] / 2) + 1); j++)
+                {
+                    if (NumratCelesa[i] % j == 0)
+                    {
+                        Array.Resize(ref NumratPerseritur, NumratPerseritur.Length + 1);
+                        NumratPerseritur[NumratPerseritur.Length - 1] = j;
+                    }
+                }
+            }
+
+            int x;
+            string q;
+            int f = MostFreq(NumratPerseritur, out x);
+            q = GjejQelesin(x);
+
+            int Gjatesia2 = NumratPerseritur.Length;
+
+            int[] GjatesiaDyte = new int[0];
+
+            for (int i = 0; i < Gjatesia2; i++)
+            {
+                if (NumratPerseritur[i] != x)
+                {
+                    Array.Resize(ref GjatesiaDyte, GjatesiaDyte.Length + 1);
+                    GjatesiaDyte[GjatesiaDyte.Length - 1] = NumratPerseritur[i];
+                }
+            }
+
+            int y;
+            string t;
+            int g = MostFreq(GjatesiaDyte, out y);
+            t = GjejQelesin(y);
+            int Gjatesia3 = GjatesiaDyte.Length;
+            int[] GjatesiaTrete = new int[0];
+
+            for (int i = 0; i < Gjatesia3; i++)
+            {
+                if (GjatesiaDyte[i] != y)
+                {
+                    Array.Resize(ref GjatesiaTrete, GjatesiaTrete.Length + 1);
+                    GjatesiaTrete[GjatesiaTrete.Length - 1] = GjatesiaDyte[i];
+                }
+            }
+
+            int z;
+            string u;
+            int h = MostFreq(GjatesiaTrete, out z);
+            u = GjejQelesin(z);
+            int Gjatesia4 = GjatesiaTrete.Length;
+            int[] GjatesiaKatert = new int[0];
+
+            for (int i = 0; i < Gjatesia4; i++)
+            {
+                if (GjatesiaTrete[i] != z)
+                {
+                    Array.Resize(ref GjatesiaKatert, GjatesiaKatert.Length + 1);
+                    GjatesiaKatert[GjatesiaKatert.Length - 1] = GjatesiaTrete[i];
+                }
+            }
+
+            int w;
+            string p;
+            int k = MostFreq(GjatesiaKatert, out w);
+            p = GjejQelesin(w);
+            lblCelsi1.Text = "Gjatesia e çelsave dhe çelesat e mundshem: \n\t Çelesi me gjatesi:" + x + " është: " +q+
+                "\n\t Çelesi me gjatesi:" + y + "  është:" + t + "\r\n\t Çelesi me gjatesi:" + z + " është:" +
+                u + "\r\n\t Çelesi me gjatesi:" + w + "  është: " + p ;
+        }
+        public static int MostFreq(int[] _M, out int x)
+        {
+            //Se pare i sortojme array-in ne ASC
+            int Max_Freq, No_Freq, i, k;
+            Array.Sort(_M);
+            k = _M[0];
+            Max_Freq = 0; i = 0; x = 0;
+            while (i < _M.Length)
+            {
+                No_Freq = 0;
+                //X eshte numri i cili shfaqet ne array Frequently
+                while (k == _M[i])
+                {
+                    No_Freq++;
+                    i++;
+                    if (i == _M.Length)
+                        break;
+                }
+                if (No_Freq > Max_Freq)
+                {
+                    Max_Freq = No_Freq;
+                    x = k;
+                }
+                if (i < _M.Length) k = _M[i];
+            }
+            return (Max_Freq);
+        }
     }
 }
